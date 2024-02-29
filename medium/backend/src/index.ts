f3f2162f0e29d9af503e976c568a9281885e6378
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-29 23:51:30 +0530

    added-prisma

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -2,41 +2,40 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.post('/api/v1/signup', (c) => {
+app.post("/api/v1/signup", (c) => {
 	return c.json({
-		message: 'Singup',
+		message: "Singup",
 	});
 });
 
-app.post('/api/v1/signin', (c) =>{
-  return c.json({
-    message: 'SingIn'
-  });
+app.post("/api/v1/signin", (c) => {
+	return c.json({
+		message: "SingIn",
+	});
 });
 
-app.post('/api/v1/blog', (c)=>{
-  return c.json({
-    message: 'Post Blog'
-  });
-})
-
-app.put('/api/v1/blog', (c)=>{
-  return c.json({
-    message: 'Get blog'
-  });
+app.post("/api/v1/blog", (c) => {
+	return c.json({
+		message: "Post Blog",
+	});
 });
 
-app.get('/api/v1/:id', (c)=>{
-  return c.json({
-    message: 'Get blog'
-  });
-})
+app.put("/api/v1/blog", (c) => {
+	return c.json({
+		message: "Get blog",
+	});
+});
 
-app.get('/api/v1/blog/bulk', (c)=>{
-  return c.json({
-    message: 'Get uld blog'
-  });
-})
+app.get("/api/v1/:id", (c) => {
+	return c.json({
+		message: "Get blog",
+	});
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+	return c.json({
+		message: "Get uld blog",
+	});
+});
 
 export default app;
-
