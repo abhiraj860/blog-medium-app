f7ffe5c47b4d2b7eb229fe5c462f43409847d4a8
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-03 23:20:01 +0530

    arrangement

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,34 +1,33 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from "hono/jwt";
-
 
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string,
-		SECRET_KEY: string
-	}
+		DATABASE_URL: string;
+		SECRET_KEY: string;
+	};
 }>();
 
-app.post('/api/v1/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL
-  }).$extends(withAccelerate());
- 	
-  const body = await c.req.json();
-  const user = await prisma.user.create({
-	data: {
-		email: body.email,
-		password: body.password,
-	},
-  })
-  return c.text("hello");
-//   const token = await sign({id: user.id}, c.env.SECRET_KEY);
-  
-//   return c.json({
-// 	jwt: token
-//   });
+app.post("/api/v1/signup", async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.create({
+		data: {
+			email: body.email,
+			password: body.password,
+		},
+	});
+	return c.text("hello");
+	//   const token = await sign({id: user.id}, c.env.SECRET_KEY);
+
+	//   return c.json({
+	// 	jwt: token
+	//   });
 });
 
 app.post("/api/v1/signin", (c) => {
@@ -60,6 +59,3 @@ app.get("/api/v1/blog/bulk", (c) => {
 		message: "Get uld blog",
 	});
 });
-
-	;
-	
