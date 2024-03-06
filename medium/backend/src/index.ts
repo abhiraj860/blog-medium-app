ea91e3922cfd9a0a4af71ae08b3c253bea099e7a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-06 23:01:19 +0530

    minor changes

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
 	Bindings: {
