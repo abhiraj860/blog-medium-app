fa228a275343838d07284709a76d6cd3b5db52d0
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-07 00:00:05 +0530

    com

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-
+// app
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string;
