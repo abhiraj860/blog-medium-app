ea11ed450c6d300a6d0596d3227d8a9e38e80805
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-08 03:22:55 +0530

    npm-pulish

--- medium-app/my-app/backend/src/routes/user.ts
+++ medium-app/my-app/backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import { signupInput, signinInput } from "@abhiraj860/medium-blog";
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -12,6 +13,13 @@ export const userRouter = new Hono<{
 
 userRouter.post('/signup', async (c) => {
   const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if(!success) {
+    c.status(411);
+    return c.json({
+      message: 'Inputs not correct'
+    });
+  }
   const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
@@ -37,6 +45,13 @@ userRouter.post('/signup', async (c) => {
 
 userRouter.post('/signin', async (c) => {
   const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if(!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs are not correct"
+    });
+  }
   const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
