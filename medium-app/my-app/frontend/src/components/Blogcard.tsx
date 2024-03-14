355120177dd362b7e3cb497137f6c8561954a311
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-15 01:19:41 +0530

    blogs-css

--- medium-app/my-app/frontend/src/components/Blogcard.tsx
+++ medium-app/my-app/frontend/src/components/Blogcard.tsx
@@ -1,6 +1,47 @@
-export const BlogCard = ()=>{
-    return <div>
+interface BlogCardProps {
+    authorName: string;
+    title: string;
+    content: string;
+    publishedDate: string;
+}
 
-        
+export const BlogCard = ({
+    authorName,
+    title,
+    content,
+    publishedDate
+}:BlogCardProps)=>{
+    return <div className="border-b p-4 border-slate-300 pb-4">
+        <div className="flex">
+            <div className="flex ">
+                <Avatar name={authorName} /> 
+            </div>
+            <div className="font-extralight pl-2 text-sm flex justify-center flex-col">{authorName}</div>
+            <div className="flex justify-center flex-col pl-2 mt-1">
+                <Circle />
+            </div>  
+            <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">{publishedDate}</div>
+        </div>
+        <div className="text-xl pt-2 font-semibold">
+            {title}
+        </div>
+        <div className="text-md font-thin">
+            {content.slice(0, 100) + "...." }
+        </div>
+        <div className="text-slate-500 font-thin pt-4 text-sm">
+            {`${Math.ceil(content.length / 100)} minute(s) read`}
+        </div>
+    </div>
+}
+
+function Avatar({name}: {name: string}) {
+       return  <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span className="text-xs  text-gray-600 dark:text-gray-300">{name[0]}</span>
+        </div>
+}
+
+function Circle() {
+    return <div className="h-1 w-1 rounded-full bg-slate-500">
+
     </div>
 }
--- medium-app/my-app/frontend/src/pages/Blogs.tsx
+++ medium-app/my-app/frontend/src/pages/Blogs.tsx
@@ -1,5 +1,28 @@
-export const Blogs = ()=> {
-    return <div>
-        Blogs
-    </div>
-}
+import { BlogCard } from "../components/Blogcard";
+
+export const Blogs = () => {
+	return (
+        <div className="flex justify-center">
+            <div className="max-w-xl"> 
+                <BlogCard
+                    authorName="Abhiraj"
+                    title="how an ugly single page website make 500$ a monthe alway."
+                    content="how an ugly single page website make 500$ a monthe alwayhow an ugly single page website make 500$ a monthe alway..first content"
+                    publishedDate="23/34/2023"
+                />
+                <BlogCard
+                    authorName="Abhiraj"
+                    title="how an ugly single page website make 500$ a monthe alway."
+                    content="how an ugly single page website make 500$ a monthe alwayhow an ugly single page website make 500$ a monthe alway..first content"
+                    publishedDate="23/34/2023"
+                />
+                <BlogCard
+                    authorName="Abhiraj"
+                    title="how an ugly single page website make 500$ a monthe alway."
+                    content="how an ugly single page website make 500$ a monthe alwayhow an ugly single page website make 500$ a monthe alway..first content"
+                    publishedDate="23/34/2023"
+                />
+            </div>
+        </div>
+	);
+};
